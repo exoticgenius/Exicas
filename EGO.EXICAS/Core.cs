0dbffdf170df123d6df5e1fd0dca40e09b26d6c5
farshad sadeghi <35816001+exoticgenius@users.noreply.github.com>
2021-05-16 18:17:32 +0430

    arranged codes

--- EGO.EXICAS/Core.cs
+++ EGO.EXICAS/Core.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.IconLib;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 using Fractal;
+using Fractal.Extensions;
 
 namespace EGO.EXICAS
 {
@@ -30,6 +35,110 @@ namespace EGO.EXICAS
         private void _collectionSaveStateIndicator(INode sender, INode parameter1)
         {
             SaveState = false;
+        }
+
+        public void SaveCollection(bool getNewPath = false)
+        {
+            string path;
+            if (getNewPath || string.IsNullOrWhiteSpace(CurrentCollection.LastSaveLocation) || !File.Exists(CurrentCollection.LastSaveLocation))
+            {
+                path = GetNewSavePath();
+                if (path is null) return;
+                CurrentCollection.LastSaveLocation = path;
+            }
+            else path = CurrentCollection.LastSaveLocation;
+
+            using StreamWriter writer = new StreamWriter(File.OpenWrite(path));
+            CurrentCollection.Collector(writer);
+        }
+        public void ReNewCollection()
+        {
+            CurrentCollection.RemoveAll();
+            CurrentCollection = new Collection();
+        }
+
+        private string GetNewSavePath()
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.DefaultExt = ".eec";
+            fileDialog.FileName = CurrentCollection.LastSaveLocation;
+            if (fileDialog.ShowDialog() != DialogResult.OK) return null;
+            return fileDialog.FileName;
+        }
+        public void OpenCollection()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".eec";
+            fileDialog.ShowDialog();
+            if (string.IsNullOrWhiteSpace(fileDialog.FileName)) return;
+
+            using StreamReader reader = new StreamReader(File.OpenRead(fileDialog.FileName));
+            string data = reader.ReadToEnd();
+            LoadCollection(data);
+        }
+
+        private void LoadCollection(string data)
+        {
+            ReNewCollection();
+            CurrentCollection.SuspendState = true;
+            CurrentCollection.Emitter(ref data);
+            CurrentCollection.SuspendState = false;
+
+        }
+
+        public void AddImage()
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Multiselect = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var item in ofd.FileNames)
+                    {
+                        Image image = Image.FromFile(item);
+                        IconImage iconImage = new IconImage();
+                        iconImage.Image = image;
+                        CurrentCollection.AddChild(iconImage);
+                    }
+                }
+            }
+        }
+
+        internal void Assemble()
+        {
+            using SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "untitled.ico";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                MultiIcon icons = new MultiIcon();
+                SingleIcon si = icons.Add("pack");
+                icons.SelectedName = "pack";
+                foreach (IconImage item in CurrentCollection.PullChildren<IconImage>())
+                {
+                    var newImage = new Bitmap(item.Image);
+                    if (newImage.Width > 256 || newImage.Height > 256)
+                    {
+                        MessageBox.Show($"icon with size larger than 256px width or 256px height cannot be added");
+                        continue;
+                    }
+                    bool notExist = true;
+                    foreach (var ei in si)
+                    {
+                        if (ei.Size.Width == newImage.Width && ei.Size.Height == newImage.Height) notExist = false;
+                    }
+                    if (notExist)
+                    {
+                        si.Add(newImage);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"icon with same size cannot be added\r\nkeep one of images with size{newImage.Width}*{newImage.Height}");
+                    }
+                }
+
+
+                icons.Save(sfd.FileName, MultiIconFormat.ICO);
+            }
         }
     }
 }
--- EGO.EXICAS/Main.cs
+++ EGO.EXICAS/Main.cs
@@ -46,6 +46,8 @@ namespace EGO.EXICAS
                 if (item.Image != parameter1) continue;
 
                 pnl_items.Controls.Remove(item);
+                (parameter1 as IconImage).Image.Dispose();
+                (parameter1 as IconImage).Features.Clear();
                 break;
             }
         }
@@ -70,12 +72,12 @@ namespace EGO.EXICAS
 
         private void MenuManager_SaveAsCollection(object sender, EventArgs e)
         {
-            SaveCollection(true);
+            Core.SaveCollection(true);
         }
 
         private void MenuManager_SaveCollection(object sender, EventArgs e)
         {
-            SaveCollection();
+            Core.SaveCollection();
         }
 
         private void MenuManager_CloseCurrentCollection(object sender, EventArgs e)
@@ -85,20 +87,12 @@ namespace EGO.EXICAS
 
         private void MenuManager_OpenCollection(object sender, EventArgs e)
         {
+            Core.OpenCollection();
+            LoadControls();
+        }
 
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.DefaultExt = ".eec";
-            fileDialog.ShowDialog();
-            if (string.IsNullOrWhiteSpace(fileDialog.FileName)) return;
-
-            using StreamReader reader = new StreamReader(File.OpenRead(fileDialog.FileName));
-            string data = reader.ReadToEnd();
-            ReNewCollection();
-            Core.CurrentCollection.SuspendState = true;
-
-            Core.CurrentCollection.Emitter(ref data);
-            Core.CurrentCollection.SuspendState = false;
-
+        private void LoadControls()
+        {
             foreach (var item in Core.CurrentCollection.PullChildren())
             {
                 Core.CurrentCollection.OnAddedChild(Core.CurrentCollection, item);
@@ -117,65 +111,26 @@ namespace EGO.EXICAS
             {
                 case DialogResult.Yes:
                     {
-                        SaveCollection();
+                        Core.SaveCollection();
                     }
                     break;
                 case DialogResult.No:
                     ReNewCollection();
                     break;
-                case DialogResult.Cancel:
-                default:
-                    return;
             }
-        }
-
-        private void SaveCollection(bool getNewPath = false)
-        {
-            string path;
-            if (getNewPath || string.IsNullOrWhiteSpace(Core.CurrentCollection.LastSaveLocation) || !File.Exists(Core.CurrentCollection.LastSaveLocation))
-            {
-                path = GetNewSavePath();
-                if (path is null) return;
-                Core.CurrentCollection.LastSaveLocation = path;
-            }
-            else path = Core.CurrentCollection.LastSaveLocation;
-
-            using StreamWriter writer = new StreamWriter(File.OpenWrite(path));
-            Core.CurrentCollection.Collector(writer);
         }
 
         private void ReNewCollection()
         {
-            Core.CurrentCollection.RemoveAll();
-            Core.CurrentCollection = new Collection();
+            Core.ReNewCollection();
             pic_preview.Image = null;
         }
         
-        private string GetNewSavePath()
-        {
-            SaveFileDialog fileDialog = new SaveFileDialog();
-            fileDialog.DefaultExt = ".eec";
-            fileDialog.FileName = Core.CurrentCollection.LastSaveLocation;
-            if (fileDialog.ShowDialog() != DialogResult.OK) return null;
-            return fileDialog.FileName;
-        }
+       
 
         private void MenuManager_AddImage(object sender, EventArgs e)
         {
-            using (OpenFileDialog ofd = new OpenFileDialog())
-            {
-                ofd.Multiselect = true;
-                if (ofd.ShowDialog() == DialogResult.OK)
-                {
-                    foreach (var item in ofd.FileNames)
-                    {
-                        Image image = Image.FromFile(item);
-                        IconImage iconImage = new IconImage();
-                        iconImage.Image = image;
-                        Core.CurrentCollection.AddChild(iconImage);
-                    }
-                }
-            }
+            Core.AddImage();
         }
 
         private void MenuManager_About(object sender, EventArgs e)
@@ -226,40 +181,7 @@ namespace EGO.EXICAS
 
         private void btn_assemble_Click(object sender, EventArgs e)
         {
-            using SaveFileDialog sfd = new SaveFileDialog();
-            sfd.FileName = "untitled.ico";
-            if (sfd.ShowDialog() == DialogResult.OK)
-            {
-                MultiIcon icons = new MultiIcon();
-                SingleIcon si = icons.Add("pack");
-                icons.SelectedName = "pack";
-                foreach (IconImage item in Core.CurrentCollection.PullChildren<IconImage>())
-                {
-                    var newImage = new Bitmap(item.Image);
-                    if (newImage.Width > 256 || newImage.Height > 256)
-                    {
-                        MessageBox.Show($"icon with size larger than 256px width or 256px height cannot be added");
-                        continue;
-                    }
-                    bool notExist = true;
-                    foreach (var ei in si)
-                    {
-                        if (ei.Size.Width == newImage.Width && ei.Size.Height == newImage.Height) notExist = false;
-                    }
-                    if (notExist)
-                    {
-                        si.Add(newImage);
-                    }
-                    else
-                    {
-                        MessageBox.Show($"icon with same size cannot be added\r\nkeep one of images with size{newImage.Width}*{newImage.Height}");
-                    }
-                }
-
-
-                icons.Save(sfd.FileName, MultiIconFormat.ICO);
-            }
-
+            Core.Assemble();
         }
     }
 }
